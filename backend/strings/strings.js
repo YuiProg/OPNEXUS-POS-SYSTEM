2f0e0558be03be3d3137d4a6358064b8266caccb
YuiProg <javierpatrick79@gmail.com>
2026-03-02 15:36:30 +0800

    clean up code

---
+++ backend/strings/strings.js
@@ -0,0 +1,27 @@
+const Strings = {
+    //server error remember to put the error message when development
+    SERVER_ERROR: 'Server error.', 
+    CRED_ERROR: 'Invalid username or password.',
+    //error messages
+    loginFail: 'Failed to login user.',
+    registerFail: 'Failed to register user.',
+    NAME_EXIST: 'A user already has this username',
+    PASS_REQ: 'Password is required.',
+    ROLE_REQ: 'User role is required',
+    BRANCH_REQ: 'Branch location is required.',
+
+    //products fail
+    FAILED_ADD: 'Failed to add product.',
+    INVALID_PRODUCT_CODE: 'Invalid product code.',
+    PRODUCT_QUANTITY: 'Please provide a quantity for product.',
+    SUCCESS_ADD: 'Product added successfully.',
+
+    //cred
+    pw: 'password',
+    nm: 'name',
+    rl: 'role',
+    br: 'branch',
+    us: 'User'
+}
+
+export default Strings;
