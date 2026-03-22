78bf5ce822db387b0a68b0cb966c8f33f7f14fbb
Octobitss <franciscomarc009@gmail.com>
2026-03-22 15:57:45 +0800

    updated Login page Styles

--- client/src/pages/AuthPage/Login.jsx
+++ client/src/pages/AuthPage/Login.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import './Login.css';
 import AuthStore from "../../store/Authstore";
 import Strings from "../../../../backend/strings/strings-codes.js";
+import InputField from "../../components/InputField/InputFIeld.jsx";
+import bg_image from "../../assets/images/ProductsLoginImage.png"
+import bg_logo from "../../assets/images/loginImageLogo.png"
 
 const {
     UNAUTHORIZED_MESS,
@@ -44,6 +47,7 @@ class Login extends React.Component {
     //WHY IS THIS IMPORTANT:
     //IMPROVES PERFORMANCE
     //DISABLES MEMORY LEAK
+    // ulol
     componentWillUnmount() {
         if (this.unsubscribe) this.unsubscribe();
     }
@@ -64,27 +68,25 @@ class Login extends React.Component {
         const { loading, username, password } = this.state;
         
         return (
-            <div>
-                <h1>Login</h1>
-                <form onSubmit={this.handleLogin}>
-                    {/* {error && error.message != UNAUTHORIZED_MESS && <p>{error.message || error}</p>} */}
-                    {/* JAVASCRIPT STRIKES AGAIN TANGINA (USNM) DI PEDE PERO (PW) PEDE AMPUTA FUCK YOU */}
-                    <input
-                        type="text"
-                        value={username}
-                        onChange={(e) => this.handleKeyChange(us.toLocaleLowerCase() + nm, e.target.value)}
-                        placeholder="Enter Username"
-                        required
-                    />
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => this.handleKeyChange(pw, e.target.value)}
-                        placeholder="Enter Password"
-                        required
-                    />
-                    <button type="submit" disabled={loading}>{loading ? 'Logging in…' : 'Login'}</button>
-                </form>
+            <div className="login-container">
+                <div className="login-container__image">
+                    <img src={bg_logo} className="login-container__image-one"> 
+                    </img>
+                    <img src={bg_image} className="login-container__image-two">
+                    </img>
+                </div>
+                <div className="login-container__credentials">
+                    <img src={bg_logo}>
+                    </img>    
+                    <form onSubmit={this.handleLogin}>
+                        <InputField required text onChange={(e) => this.setState({username: e})} placeholder={us + nm}/>
+                        <InputField required password onChange={(e) => this.setState({password: e})} placeholder={pw}/>
+                        <button type="submit" disabled={loading}>{loading ? 'Logging in…' : 'Login'}</button>
+                    </form>
+                    <p className="login-container__credentials-footer">
+                        All Rights Reserved.
+                    </p>
+                </div>
             </div>
         );
     }
