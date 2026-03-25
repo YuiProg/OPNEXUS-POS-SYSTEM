4dc1e4e3aae9cc61ca23f4635d6b259e693a29f8
bluesn0on <namuka321@outlook.com>
2026-03-25 17:21:44 +0800

    product store

--- client/src/store/ProductStore.js
+++ client/src/store/ProductStore.js
@@ -1,7 +1,11 @@
 import { create } from "zustand";
 
 const ProductStore = create((set) => ({
-
+  productName: '',
+  quantity: 0,
+  setProductData: (name, value) => {
+    set({[name]:value})
+  }
 }));
 
 export default ProductStore;
