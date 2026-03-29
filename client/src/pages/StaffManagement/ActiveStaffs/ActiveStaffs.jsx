77151fa0a9ce04a1a63e02227d79bdbd14448358
YuiProg <javierpatrick79@gmail.com>
2026-03-29 13:56:50 +0800

    staff

--- client/src/pages/StaffManagement/ActiveStaffs/ActiveStaffs.jsx
+++ client/src/pages/StaffManagement/ActiveStaffs/ActiveStaffs.jsx
@@ -2,18 +2,38 @@ import React from "react";
 import './ActiveStaffs.css'
 import { CircleUserRound } from 'lucide-react';
 
-// const staffData = [
-//   { name: 'Neg Neg', role: 'Clerk' },
-//   { name: 'Zeendy Ann Wukong', role: 'Clerk' },
-//   { name: 'Aki Tinukmol', role: 'Clerk' },
-//   { name: 'Violet Alaw Pwet', role: 'Clerk' },
-//   { name: 'Theorra Binomba', role: 'Clerk' },
-//   { name: 'Levi Inadik RP', role: 'Clerk' },
-//   { name: 'Waffle Nasampol', role: 'Clerk' },
-//   { name: 'Jaooo Tungaooo', role: 'Clerk' },
-//   { name: 'China Balyena', role: 'Clerk' },
-//   { name: 'Shiloh Kulang sa Paloh', role: 'Clerk' },
-// ];
+const staffData = [
+  { name: 'Neg Neg', role: 'Clerk' },
+  { name: 'Zeendy Ann Wukong', role: 'Clerk' },
+  { name: 'Aki Tinukmol', role: 'Clerk' },
+  { name: 'Violet Alaw Pwet', role: 'Clerk' },
+  { name: 'Theorra Binomba', role: 'Clerk' },
+  { name: 'Levi Inadik RP', role: 'Clerk' },
+  { name: 'Waffle Nasampol', role: 'Clerk' },
+  { name: 'Jaooo Tungaooo', role: 'Clerk' },
+  { name: 'China Balyena', role: 'Clerk' },
+  { name: 'Shiloh Kulang sa Paloh', role: 'Clerk' },
+  { name: 'Neg Neg', role: 'Clerk' },
+  { name: 'Zeendy Ann Wukong', role: 'Clerk' },
+  { name: 'Aki Tinukmol', role: 'Clerk' },
+  { name: 'Violet Alaw Pwet', role: 'Clerk' },
+  { name: 'Theorra Binomba', role: 'Clerk' },
+  { name: 'Levi Inadik RP', role: 'Clerk' },
+  { name: 'Waffle Nasampol', role: 'Clerk' },
+  { name: 'Jaooo Tungaooo', role: 'Clerk' },
+  { name: 'China Balyena', role: 'Clerk' },
+  { name: 'Shiloh Kulang sa Paloh', role: 'Clerk' },
+  { name: 'Neg Neg', role: 'Clerk' },
+  { name: 'Zeendy Ann Wukong', role: 'Clerk' },
+  { name: 'Aki Tinukmol', role: 'Clerk' },
+  { name: 'Violet Alaw Pwet', role: 'Clerk' },
+  { name: 'Theorra Binomba', role: 'Clerk' },
+  { name: 'Levi Inadik RP', role: 'Clerk' },
+  { name: 'Waffle Nasampol', role: 'Clerk' },
+  { name: 'Jaooo Tungaooo', role: 'Clerk' },
+  { name: 'China Balyena', role: 'Clerk' },
+  { name: 'Shiloh Kulang sa Paloh', role: 'Clerk' },
+];
 
 class ActiveStaffs extends React.Component {
   constructor(props) {
@@ -21,13 +41,14 @@ class ActiveStaffs extends React.Component {
   }
 
   render() {
-    const { staffData } = this.props;
+    // const { staffData } = this.props;
+    // const clerks = staffData.filter((d) => d.role === "Clerk");
     return (
       <div className="as-container">
         <h1 className="as-header">Active Clerks</h1>
         <ul className="staff-list">
-          {Array.isArray(staffData) && staffData.length > 0 ? (
-            staffData.map((index, idx) => {
+          {/* {Array.isArray(clerks) && clerks.length > 0 ? (
+            clerks.map((index, idx) => {
             return (
               <li key={idx} className="staff">
                 <div className="staff-icon"><CircleUserRound className="icon-pic"/></div>
@@ -38,7 +59,18 @@ class ActiveStaffs extends React.Component {
               </li>
             );
           })
-          ) : (<p>No active staff found.</p>)}
+          ) : (<p>No active clerk/s found.</p>)} */}
+          {staffData.map((index, idx) => {
+            return (
+              <li key={idx} className="staff">
+                <div className="staff-icon"><CircleUserRound className="icon-pic"/></div>
+                <div className="staff-details">
+                  <div className="staff-name">{index.username}</div>
+                  <div className="staff-role">{index.role}</div>
+                </div>
+              </li>
+            );
+          })}
         </ul>
       </div>
     )
