58a1a8991ed9eec3ed1dad93f79dd2ba7dbb0d25
Octobitss <franciscomarc009@gmail.com>
2026-03-31 13:08:49 +0800

    updated style in radio

--- client/src/components/TRRadio/Radio.jsx
+++ client/src/components/TRRadio/Radio.jsx
@@ -18,16 +18,55 @@ class Radio extends React.Component {
         } = this.props;
 
         return (
-            <div className="tr-radio-container">
-                {options.map((d, i) => {
-                    return(
-                        <>
-                        <input value={d.label.toLowerCase()} key={i} defaultChecked={defaultChecked === d.label} type="radio" name={d.name} onChange={(e) => onChange(e.target.value)}/>
-                        <label key={i + 1}>{d.label}</label>
-                        </>
-                    );
-                })}
-            </div>
+            // <div className="tr-radio-container">
+            //     {options.map((d, i) => {
+            //         return(
+            //             <>
+            //             <input value={d.label.toLowerCase()} key={i} defaultChecked={defaultChecked === d.label} type="radio" name={d.name} onChange={(e) => onChange(e.target.value)}/>
+            //             <label key={i + 1}>{d.label}</label>
+            //             </>
+            //         );
+            //     })}
+            // </div>
+                <div className="settings-demo">
+                        <div className="setting-row">
+                        <label className="setting-control">
+                            <input
+                            type="checkbox"
+                            checked={this.state.shift}
+                            onChange={() =>
+                                this.setState((prev) => ({ shift: !prev.shift }))
+                            }
+                            />
+                            <span className="switch switch--shift"></span>
+                        </label>
+
+                        <div className="setting-text">
+                            <div className="setting-title">Shift</div>
+                            <div className="setting-subtitle">Day / Night</div>
+                        </div>
+                        </div>
+
+                        <div className="setting-row">
+                        <label className="setting-control">
+                            <input
+                            type="checkbox"
+                            checked={this.state.availability}
+                            onChange={() =>
+                                this.setState((prev) => ({
+                                availability: !prev.availability,
+                                }))
+                            }
+                            />
+                            <span className="switch switch--availability"></span>
+                        </label>
+
+                        <div className="setting-text">
+                            <div className="setting-title">Availability</div>
+                            <div className="setting-subtitle">Offline / Online</div>
+                        </div>
+                        </div>
+                    </div>
         );
     }
 }
