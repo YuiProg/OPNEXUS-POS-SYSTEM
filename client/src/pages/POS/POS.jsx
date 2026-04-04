9592e6f42ad835aaf1037de949aa9591a8fe3cb5
bluesn0on <namuka321@outlook.com>
2026-04-04 09:51:07 +0800

    product cards incomplete

--- client/src/pages/POS/POS.jsx
+++ client/src/pages/POS/POS.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import './POS.css';
+import ProductCards from "../../components/ProductCards/ProductCards";
+import ProductStore from "../../context/ProductStore";
 
 class POS extends React.Component {
     constructor (props) {
         super(props);
     }
 
+    componentDidMount() {
+        const { fetchProducts, subscribeToProducts } = ProductStore.getState();
+        fetchProducts();
+        subscribeToProducts();
+    }
+
     render () {
         return (
-            <div>
-                
+            <div className="pos-container">
+                <ProductCards />
             </div>
         );
     }
