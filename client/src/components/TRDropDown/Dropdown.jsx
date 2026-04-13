3fd6972ceeca87a7976f5dfbae471582cf41e8f8
bluesn0on <namuka321@outlook.com>
2026-04-13 11:51:14 +0800

    dropdown change

--- client/src/components/TRDropDown/Dropdown.jsx
+++ client/src/components/TRDropDown/Dropdown.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import "./Dropdown.css";
-import { Store } from "lucide-react";
+import { Warehouse, ChevronDown } from "lucide-react";
 import PropTypes from "prop-types";
 
 class DropDown extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: (this.props.value && this.props.value !== "N/A") ? this.props.value : ""
+      value: (this.props.value && this.props.value !== "N/A") ? this.props.value : "",
+      isOpen: false
     };
+    this.wrapperRef = React.createRef();
   }
+
+  componentDidMount() {
+    document.addEventListener("mousedown", this.handleClickOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.handleClickOutside);
+  }
+
+  handleClickOutside = (event) => {
+    if (this.wrapperRef.current && !this.wrapperRef.current.contains(event.target)) {
+      this.setState({ isOpen: false });
+    }
+  };
 
   componentDidUpdate(prevProps) {
     if (prevProps.value !== this.props.value && this.props.value !== undefined) {
@@ -19,42 +35,58 @@ class DropDown extends React.Component {
     }
   }
 
+  toggleDropdown = () => {
+    if (!this.props.disabled) {
+      this.setState({ isOpen: !this.state.isOpen });
+    }
+  };
+
+  selectOption = (option) => {
+    this.setState({ value: option, isOpen: false });
+    this.props.onChange(option);
+  };
+
   render() {
-    const { options, defaultValue, onChange, maxWidth, customWidth, isRequired, disabled, isHeader } =
-      this.props;
+    const { options, defaultValue, isRequired, disabled, isHeader } = this.props;
+    const { value, isOpen } = this.state;
+
+    const displayValue = value || (defaultValue ? `Select ${defaultValue}...` : "Select Branch...");
 
     return (
-      <div className="tr-dropdown-wrapper">
-        <Store className="tr-dropdown-icon" />
-        <select
-          value={this.state.value}
-          className="tr-dropdown"
-          style={{
-            width: maxWidth ? "100%" : customWidth ? customWidth : "100%",
-          }}
-          onChange={(e) => {
-            this.setState({ value: e.target.value });
-            onChange(e.target.value);
-          }}
+      <div className="tr-dropdown-wrapper" ref={this.wrapperRef}>
+        <button
+          className={`tr-dropdown-button ${disabled ? "disabled" : ""}`}
+          onClick={this.toggleDropdown}
+          type="button"
           required={isRequired}
           disabled={disabled}
         >
-          <option value="" className="tr-options" disabled>
-            {defaultValue ? `Select ${defaultValue}...` : "Select Branch..."}
-          </option>
-          {isHeader && (
-            <option value="all" className="tr-options">
-              Select All
-            </option>
-          )}
-          {options?.map((l, i) => {
-            return (
-              <option className="tr-options" key={i} value={l}>
-                {l}
-              </option>
-            );
-          })}
-        </select>
+          <Warehouse className="tr-dropdown-icon" />
+          <span className="tr-dropdown-text">{displayValue}</span>
+          <ChevronDown className={`tr-dropdown-chevron ${isOpen ? "open" : ""}`} />
+        </button>
+
+        {isOpen && (
+          <ul className="tr-dropdown-list">
+            {isHeader && (
+              <li
+                className="tr-dropdown-item"
+                onClick={() => this.selectOption("all")}
+              >
+                Select All
+              </li>
+            )}
+            {options?.map((option, i) => (
+              <li
+                key={i}
+                className={`tr-dropdown-item ${value === option ? "active" : ""}`}
+                onClick={() => this.selectOption(option)}
+              >
+                {option}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     );
   }
@@ -63,11 +95,12 @@ class DropDown extends React.Component {
 DropDown.propTypes = {
   options: PropTypes.array,
   defaultValue: PropTypes.string,
-  onChange: PropTypes.func,
-  maxWidth: PropTypes.number,
-  customWidth: PropTypes.number,
+  onChange: PropTypes.func.isRequired,
+  customWidth: PropTypes.string,
   value: PropTypes.string,
   isRequired: PropTypes.bool,
+  disabled: PropTypes.bool,
+  isHeader: PropTypes.bool,
 };
 
 export default DropDown;
