8e57a31a0b25917cb92ec9f5b85080e1670bcd09
YuiProg <javierpatrick79@gmail.com>
2026-04-19 11:01:10 +0800

    added app version

--- client/src/pages/AuthPage/Login.jsx
+++ client/src/pages/AuthPage/Login.jsx
@@ -134,6 +134,7 @@ class Login extends React.Component {
                         <p className="login-container__credentials-footer">
                             All Rights Reserved.
                         </p>
+                        <p className="login-container__credentials-footer">v0.2.2</p>
                     </div>
                 </div>
             </>
