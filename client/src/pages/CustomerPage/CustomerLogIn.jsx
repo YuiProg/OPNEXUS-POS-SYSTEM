8f3dbfe3744af615d8c708e1ef4cce814ce1857b
YuiProg <javierpatrick79@gmail.com>
2026-04-19 20:21:14 +0800

    added start of customerlogin

---
+++ client/src/pages/CustomerPage/CustomerLogIn.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './CustomerLogin.css';
+
+class CustomerLogin extends React.Component {
+    render () {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default CustomerLogin;
