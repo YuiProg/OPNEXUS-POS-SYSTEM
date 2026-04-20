879c851f5f242907cb963968a21a7e9e474c46cf
bluesn0on <namuka321@outlook.com>
2026-04-20 19:52:29 +0800

    todays sales click redirects

--- client/src/components/DashboardComponents/TotalSales/TotalSales.jsx
+++ client/src/components/DashboardComponents/TotalSales/TotalSales.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './TotalSales.css';
 import totalsalesicon from '../../../assets/images/totalsalesicon.png';
+import { Link } from "react-router-dom";
 
 class TotalSales extends React.Component {
     constructor (props) {
@@ -9,12 +10,14 @@ class TotalSales extends React.Component {
 
     render () {
         return (
-            <div className="total-sales-container">
-                <img className="total-sales-icon" src={totalsalesicon} alt="totalsalesicon" />
-                <h1 className="total-sales-amount">PHP {Number(this.props.sale).toLocaleString('en-PH', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</h1>
-                <h2 className="total-sales-label">Total Sales</h2>
-                {/* <p className="total-sales-change">+8% from yesterday</p> */}
-            </div>
+            <Link to="/logs" className="total-sales-link">
+                <div className="total-sales-container">
+                    <img className="total-sales-icon" src={totalsalesicon} alt="totalsalesicon" />
+                    <h1 className="total-sales-amount">PHP {Number(this.props.sale).toLocaleString('en-PH', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</h1>
+                    <h2 className="total-sales-label">Total Sales</h2>
+                    {/* <p className="total-sales-change">+8% from yesterday</p> */}
+                </div>
+            </Link>
         );
     }
 }
