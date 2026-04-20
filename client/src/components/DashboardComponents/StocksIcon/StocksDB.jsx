cfb5cffbd5e7ca7e88ee89a89d5a24b08cd2d6c6
bluesn0on <namuka321@outlook.com>
2026-04-20 18:21:53 +0800

    stocks link

--- client/src/components/DashboardComponents/StocksIcon/StocksDB.jsx
+++ client/src/components/DashboardComponents/StocksIcon/StocksDB.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./StocksDB.css";
 import stocksicon from "../../../assets/images/stocksicon.png";
+import { Link } from "react-router-dom";
 
 class StocksDB extends React.Component {
   constructor(props) {
@@ -9,12 +10,14 @@ class StocksDB extends React.Component {
 
   render() {
     return (
-      <div className="stocks-container">
-        <img className="stocks-icon" src={stocksicon} alt="stocksicon" />
-        <h1 className="stocks-amount">{this.props.stock}</h1>
-        <h2 className="stocks-label">Stocks</h2>
-        {/* <p className="stocks-change">+1.2% from yesterday</p> */}
-      </div>
+      <Link to="/inventory" className="stocks-link">
+        <div className="stocks-container">
+          <img className="stocks-icon" src={stocksicon} alt="stocksicon" />
+          <h1 className="stocks-amount">{this.props.stock}</h1>
+          <h2 className="stocks-label">Stocks</h2>
+          {/* <p className="stocks-change">+1.2% from yesterday</p> */}
+        </div>
+      </Link>
     );
   }
 }
