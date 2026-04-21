3ee56bddc9943c94654c3d78758165e3655be5aa
Octobitss <franciscomarc009@gmail.com>
2026-04-21 22:56:39 +0800

    updated customervip

--- client/src/pages/CustomerPage/CustomerLogIn.jsx
+++ client/src/pages/CustomerPage/CustomerLogIn.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import './CustomerLogin.css';
-
+import '../AuthPage/Login.css';
 class CustomerLogin extends React.Component {
-    render () {
+    render() {
         return (
             <div>
-
+                <div className="login-container__marquee" aria-hidden="true">
+                    {Array.from({ length: 20 }).map((_, rowIndex) => (
+                        <div key={rowIndex} className="login-container__marquee-row">
+                            <div className="login-container__marquee-track">
+                                <span>RELX JUICE</span>
+                                <span>XULTRA JUICE</span>
+                                <span>PUFFS BLACK</span>
+                                <span>VAPE FLAVOR</span>
+                                <span>CLOUD CART</span>
+                                <span>RELX JUICE</span>
+                                <span>XULTRA JUICE</span>
+                                <span>PUFFS BLACK</span>
+                                <span>VAPE FLAVOR</span>
+                                <span>CLOUD CART</span>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         );
     }
