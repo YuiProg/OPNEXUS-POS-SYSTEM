e94753f4eb4f68ef384c37cf0d5a18c3b91501a5
YuiProg <javierpatrick79@gmail.com>
2026-04-21 22:59:36 +0800

    added endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -37,6 +37,7 @@ import disableManyConfirmModal from "./pages/VIP/DisableManyConfirm.jsx";
 import newCategoryModal from "./pages/Inventory/NewCategoryModal.jsx";
 import CategoryStore from "./context/CategoryStore.js";
 import deleteCategoryConfirm from "./pages/Inventory/DeleteCategoryConfirm.jsx";
+import CustomerLogin from "./pages/CustomerPage/CustomerLogIn.jsx";
 
 
 const ServerError = lazy(
@@ -1320,6 +1321,8 @@ export default function App() {
               )
             }
           />
+
+          <Route path='/customer-vip' element={<CustomerLogin/>}/>
           {/* NO URL AND ERROR */}
           <Route path="*" element={<URLError />} />
           <Route path="/servererror" element={<ServerError />} />
