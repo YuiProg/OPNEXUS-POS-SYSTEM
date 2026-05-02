e676f70cdff10731c7dbf1aa36a922dc5a3e9d70
YuiProg <javierpatrick79@gmail.com>
2026-05-02 13:32:20 +0800

    ready for release 1.0.0

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './indexMobile.css'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
       <App/>    
-  </BrowserRouter>
+  </HashRouter>
 )
