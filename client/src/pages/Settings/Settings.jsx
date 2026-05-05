cbbd21934fc5c2d92f6324b2e080ebb3433979a3
bluesn0on <namuka321@outlook.com>
2026-05-06 05:06:31 +0800

    unfinished settings

--- client/src/pages/Settings/Settings.jsx
+++ client/src/pages/Settings/Settings.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import './Settings.css';
 import { InputForm, InputRow, TRInputFormPanel } from "../../components/TRInputForm/TRInputForm";
 import InputField from "../../components/TRInputField/InputFIeld";
+import { PanelPage } from "../../components/TRPanelPage/TRPanelPage";
 
 class SettingsPage extends React.Component {
     constructor(props) {
@@ -9,17 +10,37 @@ class SettingsPage extends React.Component {
     }
 
     render () {
+        const settingsOptions = [
+            {
+                title: "Inventory"
+            },
+            {
+                title: "Categories"
+            },
+            {
+                title: "Staff Management"
+            },
+            {
+                title: "VIP Management"
+            },
+            {
+                title: "Branches"
+            }
+        ]
         return (
-            <div className="st-container">
-                <div className="st-top-contents">
-                    <div className="st-header">
-                        <h1 className="st-bigtitle">Settings</h1>
-                        <p className="st-sentence">
-                            Manage your settings and preferences.
-                        </p>
-                    </div>
+            <PanelPage 
+                titlePage="Settings" 
+                subTitle="Manage your account settings and preferences."
+            >
+                {/* dito nalang kaya natin ilagay yung "Change password" ng admin accounts */}
+                <div className="settings-options">
+                    {settingsOptions.map((option, index) => (
+                        <div key={index} className="setting-option">
+                            <h3>{option.title}</h3>
+                        </div>
+                    ))}
                 </div>
-            </div>
+            </PanelPage>
         );
     }
 }
