ba439dce1da337b3141c6471e7ee62ebc5ef1e91
bluesn0on <namuka321@outlook.com>
2026-05-07 17:15:12 +0800

    settings update

--- client/src/pages/Settings/Settings.jsx
+++ client/src/pages/Settings/Settings.jsx
@@ -3,6 +3,7 @@ import './Settings.css';
 import { InputForm, InputRow, TRInputFormPanel } from "../../components/TRInputForm/TRInputForm";
 import InputField from "../../components/TRInputField/InputFIeld";
 import { PanelPage } from "../../components/TRPanelPage/TRPanelPage";
+import Toggle from "../../components/TRToggle/Toggle";
 
 class SettingsPage extends React.Component {
     constructor(props) {
@@ -24,30 +25,123 @@ class SettingsPage extends React.Component {
                 return (
                 <div className="settings-content">
                     <h2 className="option-title">Inventory Settings</h2>
+                    <InputRow titles={["Low Stock Threshold"]}>
+                        <InputField
+                            number
+                        />
+                    </InputRow>
+                    <h4>Maximum Character Limits for Items</h4>
+                    <InputRow gap={10} titles={["Product Name", "Quantity", "Price"]}>
+                        <InputField
+                            number
+                        />
+                        <InputField
+                            number
+                        />
+                        <InputField
+                            number
+                        />
+                    </InputRow>
                 </div>
                 );
             case "Categories":
                 return (
                 <div className="settings-content">
                     <h2 className="option-title">Categories Settings</h2>
+                    <InputRow titles={["Enable"]}>
+                        <Toggle />
+                    </InputRow>
+                    <h4>Maximum Character Limits for Categories</h4>
+                    <InputRow gap={10} titles={["Category Name"]}>
+                        <InputField
+                            number
+                        />
+                    </InputRow>
                 </div>
                 );
             case "Staff Management":
                 return (
                 <div className="settings-content">
                     <h2 className="option-title">Staff Management Settings</h2>
+                    <InputRow titles={["Enable"]}>
+                        <Toggle />
+                    </InputRow>
+                    <h4>Maximum Character Limits for Staff Management</h4>
+                    <InputRow gap={10} titles={["Username", "Password"]}>
+                        <InputField
+                            number
+                        />
+                        <InputField
+                            number
+                        />
+                    </InputRow>
+                    <InputRow gap={10} titles={["First Name", "Middle Name", "Last Name"]}>
+                        <InputField
+                            number
+                        />
+                        <InputField
+                            number
+                        />
+                        <InputField
+                            number
+                        />
+                    </InputRow>
+                    <InputRow gap={10} titles={["Address", "Salary"]}>
+                        <InputField
+                            number
+                        />
+                        <InputField
+                            number
+                        />
+                    </InputRow>
                 </div>
                 );
             case "VIP Management":
                 return (
                 <div className="settings-content">
                     <h2 className="option-title">VIP Management Settings</h2>
+                    <InputRow titles={["Enable"]}>
+                        <Toggle />
+                    </InputRow>
+                    <InputRow titles={["Enable VIP ID Input Field"]}>
+                        <Toggle />
+                    </InputRow>
+                    <h4>Maximum Character Limits for VIP Management</h4>
+                    <InputRow gap={10} titles={["First Name", "Middle Name", "Last Name"]}>
+                        <InputField
+                            number
+                        />
+                        <InputField
+                            number
+                        />
+                        <InputField
+                            number
+                        />
+                    </InputRow>
+                    <InputRow gap={10} titles={["Require Email", "Require Phone Number"]}>
+                        <Toggle />
+                        <Toggle />
+                    </InputRow>
+                    <InputRow titles={["Points"]}>
+                        <InputField
+                            number
+                        />
+                    </InputRow>
                 </div>
                 );
             case "Branches":
                 return (
                 <div className="settings-content">
                     <h2 className="option-title">Branches Settings</h2>
+                    <InputRow titles={["Enable"]}>
+                        <Toggle />
+                    </InputRow>
+                    <h4>Maximum Character Limits for Branches</h4>
+                    <InputRow gap={10} titles={["Branch Name"]}>
+                        <InputField
+                            number
+                        />
+                    </InputRow>
                 </div>
                 );
             default:
