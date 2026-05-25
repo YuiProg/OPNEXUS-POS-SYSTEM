66bffb26daf093dbb1416b7289ff4609a1f29ab5
YuiProg <javierpatrick79@gmail.com>
2026-05-25 16:40:14 +0800

    removed hashrouter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './indexMobile.css'
 import App from './App'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
-      <App/>    
-  </HashRouter>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
