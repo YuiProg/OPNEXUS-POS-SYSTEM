4c67f7d3b2cbaf2ef276ffb54312a203c4b74f6a
YuiProg <javierpatrick79@gmail.com>
2026-05-25 16:13:30 +0800

    edited dockerfile

--- client/src/TRModal/Modal.jsx
+++ client/src/TRModal/Modal.jsx
@@ -173,11 +173,11 @@ export class ModalError extends React.Component {
     }
 
     render () {
-        const {
-            missingFields,
-            onClose,
-            message
-        } = this.props;
+        // const {
+        //     missingFields,
+        //     onClose,
+        //     message
+        // } = this.props;
         return (
             <div>
 
